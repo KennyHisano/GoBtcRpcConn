deefb557b008daff9af8b6c9c3c5ca4532089fc0
KennyHisano <nobi3892@gmail.com>
2018-02-10 04:29:27 +0900

    add main and done

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"flag"
+
 
 	rpc "github.com/btcsuite/btcd/rpcclient"
-	"flag"
+
+	"net"
+	"github.com/btcsuite/btcd/chaincfg"
 )
 
+
+
+var (
+	chainParams = &chaincfg.MainNetParams
+)
 
 var (
 	flagset		= flag.NewFlagSet("",flag.ExitOnError)
@@ -16,15 +25,51 @@ var (
 	testnetFlag = flagset.Bool("testnet", false, "use testnet network")
 )
 
-func main() {
+func run() (err error) {
 
+	connect, err :=normalizeAddress(*connectFlag, walletPort(chainParams))
 	connConfig := &rpc.ConnConfig{
 		Host:		connect,
 		User:		*rpcuserFlag,
 		Pass:		*rpcpassFlag,
 		DisableTLS: true,
-
+		HTTPPostMode: true,
 
 	}
+	client, err := rpc.New(connConfig,nil)
+	if err != nil {
+		return fmt.Errorf("rpc no connect")
+		}
 
+	defer func(){
+		client.Shutdown()
+		client.WaitForShutdown()
+	}()
+
+	err = fmt.Errorf("success")
+	return err
+}
+
+func normalizeAddress(addr string, defaultPort string) (hostport string, err error) {
+	host, port, origErr := net.SplitHostPort(addr)
+	if origErr == nil {
+		return net.JoinHostPort(host, port), nil
+	}
+	addr = net.JoinHostPort(addr, defaultPort)
+	_, _, err = net.SplitHostPort(addr)
+	if err != nil {
+		return "", origErr
+	}
+	return addr, nil
+}
+
+func walletPort(params *chaincfg.Params) string {
+	switch params {
+	case &chaincfg.MainNetParams:
+		return "8332"
+	case &chaincfg.TestNet3Params:
+		return "18332"
+	default:
+		return ""
+	}
 }
